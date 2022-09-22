e333e3390aa9f00a6aed5ac8ed59cc8fd3c22021
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-09-23 03:11:19 +0900

    Fix GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
-const withExportImages = require("next-export-optimize-images");
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
-module.exports = withExportImages(nextConfig);
+module.exports = nextConfig;
